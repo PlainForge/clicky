7503bd1cd0af9e182f8b7720aee229d4f94a6096
ArenJosiah Sagun <arensagun2@gmail.com>
2025-11-27 21:53:21 -0500

    Removed API key

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBdkuVrsK6GyU1qdZ_Tt5fEpJZJC5BVL4k",
+  apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: "clickyio.firebaseapp.com",
   projectId: "clickyio",
   storageBucket: "clickyio.firebasestorage.app",
